7c7d9f7561e56ab88373aa122e32efaaf57bd9f8
Glitch (hello-express) <none>
2019-10-27 22:26:23 +0000

    🎬🍈 Checkpoint ./index.js:2579398/330

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'NjM4MTI4MzM1NTUzMTY3NDM4.XbYOcw.gAzcv9laiy2wjcfDRw6fEElC4MA';
+const prefix = '!';
 
 bot.on('ready', () => {
   console.log('GorillaBot is online')
@@ -8,8 +9,10 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if(msg.content === "gorilla") {
-    msg.reply('gorilla is here');
+    msg.reply(`i'm here`);
   }
 });
 
+// this logs the bot into Discord and updates all functionality
+// if the token is stolen then request a new one on the dev site
 bot.login(token);
