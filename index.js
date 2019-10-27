d449d5f73b89b14d112e46e88089ddfe6b4942c0
Glitch (hello-express) <none>
2019-10-27 21:49:59 +0000

    🍔👖 Checkpoint ./index.js:2579398/236

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = 'NjM4MTI4MzM1NTUzMTY3NDM4.XbYOcw.gAzcv9laiy2wjcfDRw6fEElC4MA';
+
+bot.on('ready', () => console.log('GorillaBot is online'));
+bot.login(token);
