d07eb03f50299e6885d7d348ec000750474eff84
Glitch (hello-express) <none>
2023-09-24 14:23:03 -0700

    Added youtube music streaming

--- commands/uitilities/play.js
+++ commands/uitilities/play.js
@@ -2,11 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 const { createAudioPlayer, NoSubscriberBehavior } = require('@discordjs/voice');
 const { createAudioResource, StreamType } = require('@discordjs/voice');
 const { joinVoiceChannel } = require('@discordjs/voice');
-const { generateDependencyReport } = require('@discordjs/voice');
 const fs = require('node:fs');
-const { join } = require('path');
 const ytdl = require('ytdl-core');
-
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,33 +14,33 @@ module.exports = {
                 .setDescription('The URL of the song you want to play')
                 .setRequired(true)),
     async execute(interaction) {       
-        // console.log(generateDependencyReport());
-
         const url = interaction.options.getString('url');
 
         ytdl.getInfo(url).then(info => {
-            const songId = info.videoDetails.videoId;
+            const songId = info.videoDetails.title;
 
-            const player = createAudioPlayer({
-                behaviors: {
-                    noSubscriber: NoSubscriberBehavior.Pause,
-                },
+            // start downloading then reference the resource
+            ytdl(url).pipe(fs.createWriteStream(`./audio/${songId}.mp3`)).on('finish', () => {
+                const player = createAudioPlayer({
+                    behaviors: {
+                        noSubscriber: NoSubscriberBehavior.Pause,
+                    },
+                });
+
+                const resource = createAudioResource(`./audio/${songId}.mp3`, { inlineVolume: true });
+                resource.volume.setVolume(0.5);
+
+                const connection = joinVoiceChannel({
+                    channelId: interaction.member.voice.channel.id,
+                    guildId: interaction.guild.id,
+                    adapterCreator: interaction.guild.voiceAdapterCreator,
+                });
+
+                connection.subscribe(player);
+                player.play(resource);
+
+                interaction.reply(`**Now playing**\t${songId}`);
             });
-
-            let resource = createAudioResource(`./audio/${songId}.mp3`);
-            resource = createAudioResource(`./audio/${songId}.mp3`, { inlineVolume: true });
-            resource.volume.setVolume(0.5);
-
-            ytdl(url).pipe(fs.createWriteStream(`./audio/${songId}.mp3`));
-
-            const connection = joinVoiceChannel({
-                channelId: interaction.member.voice.channel.id,
-                guildId: interaction.guild.id,
-                adapterCreator: interaction.guild.voiceAdapterCreator,
-            });
-
-            connection.subscribe(player);
-            player.play(resource)
         });
     },
 };
